40e8a7880eb1afd2bd8de163e0e38a6e4961ef98
Kevin Krummnacker <kevin.krummnacker@gmail.com>
2022-04-26 00:47:41 +0200

    chore: example optional accessKey

--- examples/BasicCall.php
+++ examples/BasicCall.php
@@ -5,7 +5,7 @@ $client = new \DarthSoup\WhmcsApi\Client();
 
 // Login Credentials with Username and Password (without md5)
 $client->authenticate('admin', 'secret', \DarthSoup\WhmcsApi\Client::AUTH_LOGIN_CREDENTIALS);
-$client->accessKey('apikey');
+# $client->accessKey('apikey'); # if needed
 $client->url('http://whmcs.local:8081');
 
 var_dump($client->orders()->getOrders());
